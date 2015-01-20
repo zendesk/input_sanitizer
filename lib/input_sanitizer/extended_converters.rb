1ce8052f47c4d67a721afe3e4f0d03ea3859004b
Jarek Radosz <jradosz@gmail.com>
2015-01-20 18:10:26 +0100

    Refactor extended converters.

--- lib/input_sanitizer/extended_converters.rb
+++ lib/input_sanitizer/extended_converters.rb
@@ -1,9 +1,12 @@
 module InputSanitizer
   class PositiveIntegerConverter < IntegerConverter
     def call(value)
-      val = super
-      raise ConversionError.new("invalid integer (neagtive or zero)") if val <= 0
-      val
+      super.tap { |value| raise_error if value <= 0 }
+    end
+
+    private
+    def raise_error
+      raise ConversionError.new("invalid integer (neagtive or zero)")
     end
   end
 
@@ -11,12 +14,12 @@ module InputSanitizer
     def call(value)
       value = value.to_s
       non_valid = value.gsub(/[0-9,]/, "")
+
       if non_valid.empty?
-        parts = value.split(",").map(&:to_i)
+        value.split(",").map(&:to_i)
       else
-        invalid_chars = non_valid.split(//)
-        invalid_chars_desc = invalid_chars.join(", ")
-        raise InputSanitizer::ConversionError.new("Invalid integers: #{invalid_chars_desc}")
+        invalid_chars = non_valid.split(//).join(", ")
+        raise InputSanitizer::ConversionError.new("Invalid integers: #{invalid_chars}")
       end
     end
   end
@@ -25,12 +28,12 @@ module InputSanitizer
     def call(value)
       value = value.to_s
       non_valid = value.gsub(/[a-zA-Z,_]/, "")
+
       if non_valid.empty?
-        parts = value.split(",").map(&:to_s)
+        value.split(",").map(&:to_s)
       else
-        invalid_chars = non_valid.split(//)
-        invalid_chars_desc = invalid_chars.join(", ")
-        raise InputSanitizer::ConversionError.new("Invalid strings: #{invalid_chars_desc}")
+        invalid_chars = non_valid.split(//).join(", ")
+        raise InputSanitizer::ConversionError.new("Invalid strings: #{invalid_chars}")
       end
     end
   end
@@ -41,16 +44,14 @@ module InputSanitizer
     end
 
     def call(value)
-      found = @valid_values.include?(value) ? value : nil
-      if !found
-        found = @valid_values.include?(value.to_sym) ? value.to_sym : nil
-      end
-      if !found
-        values_joined = @valid_values.join(", ")
-        error_message = "Possible values: #{values_joined}"
-        raise InputSanitizer::ConversionError.new(error_message)
+      case
+      when @valid_values.include?(value)
+        value
+      when @valid_values.include?(value.to_sym)
+        value.to_sym
       else
-        found
+        values_joined = @valid_values.join(", ")
+        raise InputSanitizer::ConversionError.new("Possible values: #{values_joined}")
       end
     end
   end
