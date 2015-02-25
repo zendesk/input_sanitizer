e893ff58bd8adccf1b24febd9d510837a09a52e8
Jarek Radosz <jradosz@gmail.com>
2015-02-25 18:52:32 +0100

    Correct handling of nil/blank values for V2 types.

--- lib/input_sanitizer/v2/types.rb
+++ lib/input_sanitizer/v2/types.rb
@@ -1,10 +1,18 @@
+require 'active_support/core_ext/object/blank'
+
 module InputSanitizer::V2::Types
   class IntegerCheck
     def call(value, options = {})
-      Integer(value).tap do |integer|
-        raise InputSanitizer::TypeMismatchError.new(value, :integer) unless integer == value
-        raise InputSanitizer::ValueError.new(value, options[:minimum], options[:maximum]) if options[:minimum] && integer < options[:minimum]
-        raise InputSanitizer::ValueError.new(value, options[:minimum], options[:maximum]) if options[:maximum] && integer > options[:maximum]
+      if value == nil && (options[:allow_nil] == false || options[:allow_blank] == false || options[:required] == true)
+        raise InputSanitizer::ValueMissingError
+      elsif value == nil
+        value
+      else
+        Integer(value).tap do |integer|
+          raise InputSanitizer::TypeMismatchError.new(value, :integer) unless integer == value
+          raise InputSanitizer::ValueError.new(value, options[:minimum], options[:maximum]) if options[:minimum] && integer < options[:minimum]
+          raise InputSanitizer::ValueError.new(value, options[:minimum], options[:maximum]) if options[:maximum] && integer > options[:maximum]
+        end
       end
     rescue ArgumentError, TypeError
       raise InputSanitizer::TypeMismatchError.new(value, :integer)
@@ -23,16 +31,26 @@ module InputSanitizer::V2::Types
   end
 
   class StringCheck
-    def call(value)
-      value.to_s.tap do |string|
-        raise InputSanitizer::TypeMismatchError.new(value, :string) unless string == value
+    def call(value, options = {})
+      if value.blank? && (options[:allow_blank] == false || options[:required] == true)
+        raise InputSanitizer::ValueMissingError
+      elsif value == nil && options[:allow_nil] == false
+        raise InputSanitizer::ValueMissingError
+      elsif value.blank?
+        value
+      else
+        value.to_s.tap do |string|
+          raise InputSanitizer::TypeMismatchError.new(value, :string) unless string == value
+        end
       end
     end
   end
 
   class BooleanCheck
-    def call(value)
-      if [true, false].include?(value)
+    def call(value, options = {})
+      if value == nil
+        raise InputSanitizer::ValueMissingError
+      elsif [true, false].include?(value)
         value
       else
         raise InputSanitizer::TypeMismatchError.new(value, :boolean)
@@ -41,7 +59,7 @@ module InputSanitizer::V2::Types
   end
 
   class CoercingBooleanCheck
-    def call(value)
+    def call(value, options = {})
       if [true, 'true'].include?(value)
         true
       elsif [false, 'false'].include?(value)
@@ -53,19 +71,36 @@ module InputSanitizer::V2::Types
   end
 
   class DatetimeCheck
-    def call(value)
-      DateTime.parse(value)
+    def call(value, options = {})
+      if value.blank? && (options[:allow_blank] == false || options[:required] == true)
+        raise InputSanitizer::ValueMissingError
+      elsif value == nil && options[:allow_nil] == false
+        raise InputSanitizer::ValueMissingError
+      elsif value.blank?
+        value
+      else
+        DateTime.parse(value)
+      end
+
     rescue ArgumentError
       raise InputSanitizer::TypeMismatchError.new(value, :datetime)
     end
   end
 
   class URLCheck
-    def call(value)
-      unless /\A#{URI.regexp(%w(http https)).to_s}\z/.match(value)
-        raise InputSanitizer::TypeMismatchError.new(value, :url)
+    def call(value, options = {})
+      if value.blank? && (options[:allow_blank] == false || options[:required] == true)
+        raise InputSanitizer::ValueMissingError
+      elsif value == nil && options[:allow_nil] == false
+        raise InputSanitizer::ValueMissingError
+      elsif value.blank?
+        value
+      else
+        unless /\A#{URI.regexp(%w(http https)).to_s}\z/.match(value)
+          raise InputSanitizer::TypeMismatchError.new(value, :url)
+        end
+        value
       end
-      value
     end
   end
 end
---
+++ spec/v2/converters_spec.rb
@@ -0,0 +1,115 @@
+require 'spec_helper'
+
+# OPTIONS
+# :allow_nil (default true)
+# :allow_blank (default true)
+# :require (default false, requires given key in params, and additionally sets :allow_nil and :allow_blank to false)
+
+#           | allow_nil | allow_blank
+# __________|___________|_____________
+# string    |    YES    |     YES
+# url       |    YES    |     YES
+# datetime  |    YES    |     YES
+# integer   |    YES    |     NO
+# boolean   |    NO     |     NO
+# ------------------------------------
+
+# [type, value, option] => valid? # if valid? == false then check for ValueMissing error
+
+test_cases = {
+  [InputSanitizer::V2::Types::StringCheck, { :allow_nil => false }, nil] => false,
+  [InputSanitizer::V2::Types::StringCheck, { :allow_nil => false }, ""] => true,
+  [InputSanitizer::V2::Types::StringCheck, { :allow_nil => false }, "zz"] => true,
+  [InputSanitizer::V2::Types::StringCheck, { :allow_blank => false }, nil] => false,
+  [InputSanitizer::V2::Types::StringCheck, { :allow_blank => false }, ""] => false,
+  [InputSanitizer::V2::Types::StringCheck, { :allow_blank => false }, "zz"] => true,
+  [InputSanitizer::V2::Types::StringCheck, { :required => true }, nil] => false,
+  [InputSanitizer::V2::Types::StringCheck, { :required => true }, ""] => false,
+  [InputSanitizer::V2::Types::StringCheck, { :required => true }, "zz"] => true,
+  [InputSanitizer::V2::Types::StringCheck, { }, nil] => true,
+  [InputSanitizer::V2::Types::StringCheck, { }, ""] => true,
+  [InputSanitizer::V2::Types::StringCheck, { }, "zz"] => true,
+  [InputSanitizer::V2::Types::IntegerCheck, { :allow_nil => false }, nil] => false,
+  [InputSanitizer::V2::Types::IntegerCheck, { :allow_nil => false }, ""] => :invalid_type,
+  [InputSanitizer::V2::Types::IntegerCheck, { :allow_nil => false }, 1] => true,
+  [InputSanitizer::V2::Types::IntegerCheck, { :allow_blank => false }, nil] => false,
+  [InputSanitizer::V2::Types::IntegerCheck, { :allow_blank => false }, ""] => :invalid_type,
+  [InputSanitizer::V2::Types::IntegerCheck, { :allow_blank => false }, 1] => true,
+  [InputSanitizer::V2::Types::IntegerCheck, { :required => true }, nil] => false,
+  [InputSanitizer::V2::Types::IntegerCheck, { :required => true }, ""] => :invalid_type,
+  [InputSanitizer::V2::Types::IntegerCheck, { :required => true }, 1] => true,
+  [InputSanitizer::V2::Types::IntegerCheck, { }, nil] => true,
+  [InputSanitizer::V2::Types::IntegerCheck, { }, ""] => :invalid_type,
+  [InputSanitizer::V2::Types::IntegerCheck, { }, 1] => true,
+  [InputSanitizer::V2::Types::BooleanCheck, { :allow_nil => false }, nil] => false,
+  [InputSanitizer::V2::Types::BooleanCheck, { :allow_nil => false }, ""] => :invalid_type,
+  [InputSanitizer::V2::Types::BooleanCheck, { :allow_nil => false }, true] => true,
+  [InputSanitizer::V2::Types::BooleanCheck, { :allow_blank => false }, nil] => false,
+  [InputSanitizer::V2::Types::BooleanCheck, { :allow_blank => false }, ""] => :invalid_type,
+  [InputSanitizer::V2::Types::BooleanCheck, { :allow_blank => false }, true] => true,
+  [InputSanitizer::V2::Types::BooleanCheck, { :required => true }, nil] => false,
+  [InputSanitizer::V2::Types::BooleanCheck, { :required => true }, ""] => :invalid_type,
+  [InputSanitizer::V2::Types::BooleanCheck, { :required => true }, true] => true,
+  [InputSanitizer::V2::Types::BooleanCheck, { }, nil] => false,
+  [InputSanitizer::V2::Types::BooleanCheck, { }, ""] => :invalid_type,
+  [InputSanitizer::V2::Types::BooleanCheck, { }, true] => true,
+  [InputSanitizer::V2::Types::DatetimeCheck, { :allow_nil => false }, nil] => false,
+  [InputSanitizer::V2::Types::DatetimeCheck, { :allow_nil => false }, ""] => true,
+  [InputSanitizer::V2::Types::DatetimeCheck, { :allow_nil => false }, "2014-08-27T16:32:56Z"] => true,
+  [InputSanitizer::V2::Types::DatetimeCheck, { :allow_blank => false }, nil] => false,
+  [InputSanitizer::V2::Types::DatetimeCheck, { :allow_blank => false }, ""] => false,
+  [InputSanitizer::V2::Types::DatetimeCheck, { :allow_blank => false }, "2014-08-27T16:32:56Z"] => true,
+  [InputSanitizer::V2::Types::DatetimeCheck, { :required => true }, nil] => false,
+  [InputSanitizer::V2::Types::DatetimeCheck, { :required => true }, ""] => false,
+  [InputSanitizer::V2::Types::DatetimeCheck, { :required => true }, "2014-08-27T16:32:56Z"] => true,
+  [InputSanitizer::V2::Types::DatetimeCheck, { }, nil] => true,
+  [InputSanitizer::V2::Types::DatetimeCheck, { }, ""] => true,
+  [InputSanitizer::V2::Types::DatetimeCheck, { }, "2014-08-27T16:32:56Z"] => true,
+  [InputSanitizer::V2::Types::URLCheck, { :allow_nil => false }, nil] => false,
+  [InputSanitizer::V2::Types::URLCheck, { :allow_nil => false }, ""] => true,
+  [InputSanitizer::V2::Types::URLCheck, { :allow_nil => false }, "http://getbase.com"] => true,
+  [InputSanitizer::V2::Types::URLCheck, { :allow_blank => false }, nil] => false,
+  [InputSanitizer::V2::Types::URLCheck, { :allow_blank => false }, ""] => false,
+  [InputSanitizer::V2::Types::URLCheck, { :allow_blank => false }, "http://getbase.com"] => true,
+  [InputSanitizer::V2::Types::URLCheck, { :required => true }, nil] => false,
+  [InputSanitizer::V2::Types::URLCheck, { :required => true }, ""] => false,
+  [InputSanitizer::V2::Types::URLCheck, { :required => true }, "http://getbase.com"] => true,
+  [InputSanitizer::V2::Types::URLCheck, { }, nil] => true,
+  [InputSanitizer::V2::Types::URLCheck, { }, ""] => true,
+  [InputSanitizer::V2::Types::URLCheck, { }, "http://getbase.com"] => true,
+}
+
+describe 'type checks' do
+  test_cases.each do |(test_case, result)|
+    describe test_case[0] do
+      context "with options: #{test_case[1]}" do
+        show_value = case test_case[2]
+        when nil then 'nil'
+        when '' then "''"
+        else "'#{test_case[2]}'"
+        end
+
+        it "correctly handles value #{show_value}" do
+          case result
+          when true
+            lambda { test_case[0].new.call(test_case[2], test_case[1]) }.should_not raise_error
+          when false
+            lambda { test_case[0].new.call(test_case[2], test_case[1]) }.should raise_error(InputSanitizer::ValueMissingError)
+          when :invalid_type
+            lambda { test_case[0].new.call(test_case[2], test_case[1]) }.should raise_error(InputSanitizer::TypeMismatchError)
+          end
+        end
+      end
+    end
+  end
+end
+
+# Generating test cases:
+
+# types = ['string', 'integer', 'boolean', 'datetime', 'url']
+# options = [:allow_nil_false, :allow_blank_false, :required_true, :no_params]
+# values = [nil, '', lambda { |type| { 'string' => 'zz', 'integer' => 1, 'boolean' => true, 'datetime' => '2014-08-27T16:32:56Z', 'url' => 'http://getbase.com' }[type] }]
+# test_cases = types.product(options).product(values).map(&:flatten).map! do |test_case|
+#   test_case[2] = test_case[2].call(test_case[0]) if test_case[2].is_a?(Proc); test_case
+# end
+# test_cases.each { |test_case| puts "#{test_case.inspect} => []," }; nil
