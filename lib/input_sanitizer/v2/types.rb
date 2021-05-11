928d90d63c35aa417294f1cce1d6e5034945dd9a
Marcin Tuleja <mtuleja@zendesk.com>
2021-05-11 15:24:07 +0200

    Add initial implementation for InputSanitizer::V2

--- lib/input_sanitizer/v2/types.rb
+++ lib/input_sanitizer/v2/types.rb
@@ -93,6 +93,22 @@ module InputSanitizer::V2::Types
           raise InputSanitizer::ValueError.new(value, options[:minimum], options[:maximum]) if options[:minimum] && string.length < options[:minimum]
           raise InputSanitizer::ValueError.new(value, options[:minimum], options[:maximum]) if options[:maximum] && string.length > options[:maximum]
         end
+        options[:strip_4byte_chars] ? strip_4byte_chars(value) : value
+      end
+    end
+
+    private
+
+    # Body of this method was copied from Demoji lib, see:
+    # https://github.com/taskrabbit/demoji/blob/c1e7a771da2267cbcf46f96ee113ce6824ae12f8/lib/demoji.rb#L39:L50
+    def strip_4byte_chars(string)
+      "".tap do |output_string|
+        # for instead of split and joins for perf
+        for i in (0...string.length)
+          char = string[i]
+          char = 32.chr if char.ord > 65535
+          output_string << char
+        end
       end
     end
   end
